3c6c1ab106f81486686ef05fde8e5e38f6bfde36
Andrew Kim <jeesoo2002@gmail.com>
2018-12-25 20:33:54 -0800

    setup styled comp, change elem per class, ch 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component, Fragment } from "react";
-import Button from "./Button";
+import styled from "styled-components";
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-size: 20px;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#c0392b" : "#27ae60")};
+`;
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <button className="button--success button">Success</button>
-        <button className="button--danger button">Danger</button>
+        <Button success>Success</Button>
+        <Button danger>Danger</Button>
       </Fragment>
     );
   }

930b73f9de689515c77f7837ca946d73ddfa163f
developer <jeesoo2002@gmail.com>
2019-01-03 11:35:35 -0800

    theme implemented, ch 6 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, css, keyframes } from "styled-components";
+import styled, {
+  createGlobalStyle,
+  css,
+  ThemeProvider
+} from "styled-components";
+import theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -11,62 +16,30 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-// const Button = styled.button`
-//   border-radius: 50px;
-//   padding: 5px;
-//   min-width: 120px;
-//   color: white;
-//   font-size: 20px;
-//   font-weight: 600;
-//   -webkit-appearance: none;
-//   cursor: pointer;
-//   &:active,
-//   &:focus {
-//     outline: none;
-//   }
-//   background-color: ${props => (props.danger ? "#c0392b" : "#27ae60")};
-//   ${props => {
-//     if (props.danger) {
-//       return css`
-//         animation: ${rotation} ${props.rotationTime}s linear infinite;
-//       `;
-//     }
-//   }}
-// `;
-
-// const Anchor = styled(Button)`
-//   text-decoration: none;
-// `;
-
-// const rotation = keyframes`
-//   from {
-//     tranform: rotate(0deg)
-//   }
-//   to {
-//     transform: rotate(360deg)
-//   }
-// `;
-
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
+const Card = styled.div`
+  background-color: lightblue;
 `;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  ${awesomeCard};
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 class App extends Component {
   render() {
     return (
       <>
         <GlobalStyle />
-        <Input placeholder="hello" />
+        <ThemeProvider theme={theme}>
+          <Form />
+        </ThemeProvider>
       </>
     );
   }
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  mainColor: "#ecf0f1",
+  dangerColor: "#e74c3c",
+  successColor: "#2ecc71"
+};
+
+export default theme;
